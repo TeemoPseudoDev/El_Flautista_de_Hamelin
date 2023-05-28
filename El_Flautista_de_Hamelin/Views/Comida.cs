630bf6ad3183f81eaa3f50b3da0dcfff4583d02a
MarinelaMontilla <marinelamontillamendez@gmail.com>
2023-05-28 13:26:21 -0300

    vistaComida

---
+++ El_Flautista_de_Hamelin/Views/Comida.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace El_Flautista_de_Hamelin.Views
+{
+    public partial class Comida : Form
+    {
+        public Comida()
+        {
+            InitializeComponent();
+        }
+    }
+}
